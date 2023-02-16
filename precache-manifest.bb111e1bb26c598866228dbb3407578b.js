f3612913997588f3258a4ce06e38a4012ce91c16
elixirevo <elixirevocation@gmail.com>
2023-02-16 16:42:02 +0900

    Updates

---
+++ precache-manifest.bb111e1bb26c598866228dbb3407578b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f28a9219f92a6651c3ab295980fb5c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "709f12ed6a4fd4e82965",
+    "url": "/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "4c4839a4a3c6cf53492c",
+    "url": "/static/js/2.9df98a0c.chunk.js"
+  },
+  {
+    "revision": "679dc0b0298984b03ab372d214526e9a",
+    "url": "/static/js/2.9df98a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709f12ed6a4fd4e82965",
+    "url": "/static/js/main.86bd83b3.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  },
+  {
+    "revision": "8b63b6d0db62f70b6af4e5a930506837",
+    "url": "/static/media/popcatex-logo.8b63b6d0.png"
+  }
+]);
